80082782fc5f9eafb497f7121b6d640e179173d6
hafiz1379 <hafizrasa1379@gmail.com>
2023-05-24 15:40:52 +0430

    Import tasks.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import { refresh, addYourData } from './modules/tasks.js';
+
+const refreshBtn = document.querySelector('.refresh');
+refreshBtn.addEventListener('click', () => {
+  refresh();
+});
+
+const submitBtn = document.querySelector('.submitBtn');
+submitBtn.addEventListener('click', () => {
+  addYourData();
+});
